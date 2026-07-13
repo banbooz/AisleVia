18e4afa904019582a8595cd39c7aff0a551a51cf
banbooz <banboozplays@gmail.com>
2026-07-13 21:56:47 +0100

    Add digital twin map data model

---
+++ android/app/src/main/java/com/aislevia/app/model/StoreMap.kt
@@ -0,0 +1,52 @@
+package com.aislevia.app.model
+
+import com.google.ar.core.Pose
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PoseRecord(
+    val tx: Float,
+    val ty: Float,
+    val tz: Float,
+    val qx: Float,
+    val qy: Float,
+    val qz: Float,
+    val qw: Float
+) {
+    fun toPose(): Pose = Pose(
+        floatArrayOf(tx, ty, tz),
+        floatArrayOf(qx, qy, qz, qw)
+    )
+
+    companion object {
+        fun fromPose(pose: Pose): PoseRecord {
+            val t = pose.translation
+            val q = pose.rotationQuaternion
+            return PoseRecord(t[0], t[1], t[2], q[0], q[1], q[2], q[3])
+        }
+    }
+}
+
+@Serializable
+data class LandmarkRecord(
+    val id: String,
+    val name: String,
+    val imageFile: String,
+    val physicalWidthMetres: Float,
+    val mapPose: PoseRecord
+)
+
+@Serializable
+data class ItemRecord(
+    val id: String,
+    val name: String,
+    val mapPose: PoseRecord
+)
+
+@Serializable
+data class StoreMap(
+    val version: Int = 1,
+    val name: String = "Living-room proof shop",
+    val landmarks: List<LandmarkRecord> = emptyList(),
+    val items: List<ItemRecord> = emptyList()
+)
